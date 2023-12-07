0ca39d4327c24d346c1671aa6349b8d103a13277
Kunal <mundadakunal25@gmail.com>
2023-12-07 12:09:54 -0800

    Updated code for Radar chart

--- frontend/src/components/Radar.vue
+++ frontend/src/components/Radar.vue
@@ -13,17 +13,24 @@ import { ComponentSize, Margin } from '../types';
 import { schemeCategory10 } from 'd3-scale-chromatic';
 
 interface Toxicity {
-    hate: number;
-    harassment: number;
-    self_harm: number;
-    sexual: number;
-    violence: number;
+    hate: number,
+    harassment: number,
+    self_harm: number,
+    sexual: number,
+    violence: number,
 }
+
+interface OAI {
+    message_id: string;
+    openai_moderation: string;
+}
+
 
 export default {
     data() {
         return {
-            chartData: [] as Toxicity[],
+            chartData: {} as Toxicity,
+            preprocessData: {} as OAI,
             size: { width: 0, height: 0 } as ComponentSize,
             margin: { left: 50, right: 50, top: 50, bottom: 50 } as Margin,
         }
@@ -34,20 +41,76 @@ export default {
         },
     },
     async created() {
-        const rawData = await d3.csv("../../data/palm-2.csv");
+        const rawData = await d3.csv("../../data/proc/palm-2.csv");
         let parsedData = rawData.map(d => ({
             message_id: d.message_id,
             openai_moderation: d.openai_moderation,
         }));
-        console.log(rawData[0]);
+        // console.log(rawData[0]["openai_moderation"]);
+        // const example = "{'categories': {'harassment': False, 'harassment/threatening': False, 'hate': False, 'hate/threatening': False, 'self-harm': False, 'self-harm/instructions': False, 'self-harm/intent': False, 'sexual': False, 'sexual/minors': False, 'violence': False, 'violence/graphic': False}, 'category_scores': {'harassment': 4.2268514e-08, 'harassment/threatening': 1.968493e-09, 'hate': 1.574229e-08, 'hate/threatening': 9.697849e-11, 'self-harm': 4.6034412e-11, 'self-harm/instructions': 8.869029e-12, 'self-harm/intent': 8.679583e-13, 'sexual': 4.3375917e-07, 'sexual/minors': 5.3620926e-07, 'violence': 2.966025e-06, 'violence/graphic': 1.4624901e-06}, 'flagged': False}";
+        this.preprocessData = parsedData.filter((d) => d.message_id == "1e7844b921d44b798f59deb92b7d41da_9")[0];
+        let processData = this.preprocess((this.preprocessData.openai_moderation).toString());
+        console.log(processData)
+        this.chartData = processData;
     },
     methods: {
         onResize() {
-            let target = this.$refs.donutContainer as HTMLElement;
+            let target = this.$refs.radarContainer as HTMLElement;
             if (target === undefined) return;
             this.size = { width: target.clientWidth, height: target.clientHeight };
         },
+        preprocess(inputString: string): Toxicity {
+            // console.log(this.preprocessData);
+            // const inputString = this.preprocessData.openai_moderation
+
+            // example
+            // const inputString = "{'categories': {'harassment': False, 'harassment/threatening': False, 'hate': False, 'hate/threatening': False, 'self-harm': False, 'self-harm/instructions': False, 'self-harm/intent': False, 'sexual': False, 'sexual/minors': False, 'violence': False, 'violence/graphic': False}, 'category_scores': {'harassment': 4.2268514e-08, 'harassment/threatening': 1.968493e-09, 'hate': 1.574229e-08, 'hate/threatening': 9.697849e-11, 'self-harm': 4.6034412e-11, 'self-harm/instructions': 8.869029e-12, 'self-harm/intent': 8.679583e-13, 'sexual': 4.3375917e-07, 'sexual/minors': 5.3620926e-07, 'violence': 2.966025e-06, 'violence/graphic': 1.4624901e-06}, 'flagged': False}";
+
+            // Step 1: Replace single quotes with double quotes
+            // let jsonString = inputString.replace(/'/g, '"');
+            let jsonString = inputString.replace(/False/g, 'false').replace(/True/g, 'true').replace(/'/g, '"');
+
+            console.log("JSON String");
+            console.log(jsonString);
+
+
+            // Step 2: Parse the string as JSON
+            console.log("JSON Object");
+            const jsonObject: {
+                categories: Record<string, boolean>,
+                category_scores: Record<string, number>,
+                flagged: boolean
+            } = JSON.parse(jsonString);
+            console.log(jsonObject);
+            console.log(typeof jsonObject);
+
+
+
+            // Step 3: Find maximum values for categories and their sub-categories
+            const maxValues: Record<string, number> = {};
+            for (const category in jsonObject.category_scores) {
+                const subCategories = Object.keys(jsonObject.category_scores).filter(key => key.startsWith(category + '/'));
+                const values = [jsonObject.category_scores[category], ...subCategories.map(subCategory => jsonObject.category_scores[subCategory])];
+                maxValues[category] = +Math.max(...values).toFixed(3);
+            }
+            console.log("Max values");
+            console.log(maxValues);
+
+            // Step 4: Create a new object conforming to the Toxicity interface
+            const toxicityResult: Toxicity = {
+                hate: maxValues['hate'],
+                harassment: maxValues['harassment'],
+                self_harm: maxValues['self-harm'],
+                sexual: maxValues['sexual'],
+                violence: maxValues['violence'],
+            };
+            console.log(toxicityResult);
+
+            // Step 5: Return the output object
+            return toxicityResult;
+        },
         initChart() {
+            const data = {"palm2":this.chartData};
             d3.select('#radar-svg').selectAll('*').remove()
             let svgWidth = this.size.width;
             let svgHeight = this.size.height;
@@ -146,12 +209,17 @@ export default {
                 .attr("font-size", "10px");
 
 
-
-            for (let key in this.chartData) {
-                if (this.chartData.hasOwnProperty(key)) {
-                    let elementData = this.chartData[key];
+// error is in this block, mostly due to changes in data format.
+// initial chartData format : [ Grass: {HP: 100, Attack: 100, Defense: 100, Sp_Atk: 100, Sp_Def: 100, Catch_Rate: 100, Speed: 100, count: 100}, ...]
+// current chartData format : {hate: 0.3, harassment: 0.1, self_harm:0.3, sexual: 0.5, violence: 0.2}
+// const data = this.chartData[0]
+            for (let key in data) { // initial : Grass
+                console.log("Key: ", key);
+                if (true) { // checks for the key
+                    console.log("Key in if: ", key);
+                    let elementData = data.palm2; // initial : {HP: 100, Attack: 100, Defense: 100, Sp_Atk: 100, Sp_Def: 100, Catch_Rate: 100, Speed: 100, count: 100}
                     console.log(elementData);
-                    let elementValues = Object.values(elementData);
+                    let elementValues = Object.values(elementData); // returns values as array
                     console.log(elementValues);
 
                     let numElements = elementValues.length;
@@ -168,7 +236,7 @@ export default {
                     }
 
                     let polygonVertices = elementPositions.map(function (pos, i) {
-                        let scaledValue = 1 + (elementValues[i] / 100) * 100;
+                        let scaledValue: number = 1 + (elementValues[i]/100) * 100 ;
                         return [
                             centerX + (circleRadius * scaledValue / 130) * Math.cos(i * angleOffset),
                             centerY + (circleRadius * scaledValue / 130) * Math.sin(i * angleOffset)
@@ -176,9 +244,12 @@ export default {
                     });
 
                     polygonVertices.push(polygonVertices[0]);
+                    console.log(polygonVertices);
+                    
 
                     let color = colors[colorIndex % colors.length];
-                    colorIndex++;
+                    // colorIndex++;
+
 
                     svg.append("path")
                         .datum(polygonVertices)
@@ -186,7 +257,7 @@ export default {
                         .attr("fill", color)
                         .attr("stroke", color)
                         .attr("class", "polygon")
-                        .attr("data-key", key);
+                        .attr("data-key", "Palm2");
                 }
             }
 
@@ -199,7 +270,7 @@ export default {
                 .attr("transform", "translate(" + legendX + "," + legendY + ")");
 
             let legendLabels = legend.selectAll(".legend-label")
-                .data(Object.keys(this.chartData))
+                .data(Object.keys(data))
                 .enter().append("g")
                 .attr("class", "legend-label")
                 .attr("data-key", function (d) {
@@ -232,17 +303,29 @@ export default {
                 .attr("text-anchor", "middle")
                 .attr("font-size", "24px")
                 .attr("font-weight", "bold")
-                .text("Stat Spread Per Pokémon Type");
+                .text("Message Toxicity");
 
             // addListener(svg, data, keyText, ".polygon");
             // addListener(svg, data, keyText, ".legend-label");
         },
     },
+    watch: {
+        rerender(newSize) {
+            if (!isEmpty(newSize)) {
+                d3.select('#radar-svg').selectAll('*').remove()
+                this.initChart()
+            }
+        },
+    },
+    mounted() {
+        window.addEventListener('resize', debounce(this.onResize, 100))
+        this.onResize()
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.onResize)
+    }
 };
 </script>
-
-
-
 
 <style scoped>
 .chart-container-radar {
