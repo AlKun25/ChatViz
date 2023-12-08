ee70db46fe0d5dc36f9315b59a132d44093e63b2
Ty Feng <ty@radiancelabs.com>
2023-12-07 18:52:00 -0800

    fix cors issue with backend server

--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,15 @@
-from flask import Flask, request, jsonify
 from sentiment_analysis import sentiment_analysis
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
 
 @app.route('/endpoint', methods=['GET'])
 def get_endpoint():
     input_string = request.args.get('input_string', '')
 
-    if input_string is None:
+    if input_string is None or input_string == '':
         return jsonify({'error': 'Input string is missing'}), 400
 
     # Process the input_string as needed
